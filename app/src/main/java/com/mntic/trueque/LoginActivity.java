0f7c3701cfecc3a4be9e02bc006158fda5eb22d3
JosephToq <steventoq@hotmail.com>
2021-12-05 16:01:44 -0500

    Agrego el login con google

--- app/src/main/java/com/mntic/trueque/LoginActivity.java
+++ app/src/main/java/com/mntic/trueque/LoginActivity.java
@@ -10,14 +10,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,6 +39,10 @@ public class LoginActivity extends AppCompatActivity {
     private TextView newUserTV;
     private FirebaseAuth mAuth;
     private ProgressBar loadingPB;
+    Button ingresargoogle;
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +55,11 @@ public class LoginActivity extends AppCompatActivity {
         newUserTV = findViewById(R.id.idTVNewUser);
         mAuth = FirebaseAuth.getInstance();
         loadingPB = findViewById(R.id.idPBLoading);
+        ingresargoogle = findViewById(R.id.ingresargoogle);
+
+        /*Creamos la solicitud para iniciar sesión con google*/
+        crearSolicitud();
+
         //adding click listner for our new user tv.
         newUserTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,7 +107,102 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        /*Evento al precionar el boton de iniciar con google*/
+        ingresargoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { singIn();
+            }
+        });
+
     }
+
+    /*Solicitud para google*/
+    private void crearSolicitud() {
+        //Configuramos el inicio de sesión de google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        //Creamos un google sign in con las opciones especificas de google
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+    }
+
+    //Creamos la pantalla del google
+    private void singIn(){
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Resultado devuelto al iniciar la intencion desde googlesignApi.getsignIntent
+        if (requestCode == RC_SIGN_IN ){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                //el inicio de sesion fue exitoso, autentiqqeu con firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AutenticacionFirebase(account);//aqui se ejecuta el metodo para logearnos con google
+            }catch (ApiException e){
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /*Metodo para autenticar con firebase google*/
+    private void AutenticacionFirebase(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //si inicia correctamente
+                            FirebaseUser user = mAuth.getCurrentUser();//obtenemos al usuario, el cual quiere iniciar sesion
+
+                            /*Si el usuario inicia sesion por primera vez*/
+                            if (task.getResult().getAdditionalUserInfo().isNewUser()){
+
+
+
+                                String uid = user.getUid();
+                                String email = user.getEmail();
+                                String name = user.getDisplayName();
+
+
+                                //aqui pasamos los parametros
+                                /*Creamos un hashmap para enviar los datos a firebase*/
+
+                                HashMap<Object,String> DatosUsuario = new HashMap<>();
+
+                                DatosUsuario.put("uid",uid);
+                                DatosUsuario.put("email",email);
+                                DatosUsuario.put("name",name);
+                                /*Dejo la imagen vacia*/
+                                DatosUsuario.put("image","");
+
+                                /*Iniciamos la instancia a la base de datos*/
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                /*Creamos la base de datos*/
+                                DatabaseReference reference = database.getReference("Usuarios_de_la_app");
+                                /*El nombre de la base de datos es Ususarios_de_la_app*/
+                                reference.child(uid).setValue(DatosUsuario);
+
+                            }
+                            //Nos dirija al main activity
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+
 
     @Override
     protected void onStart() {
